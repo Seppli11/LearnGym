f8c9134b4194029485d304f07ba8ecffb0b649b4
Sebastian Zumbrunn <sebastian.zumbrunn@gmail.com>
2019-07-01 16:52:28 +0200

    LG-1 add javadoc

--- src/test/java/ninja/seppli/learngym/TestStudent.java
+++ src/test/java/ninja/seppli/learngym/TestStudent.java
@@ -10,11 +10,20 @@ import ninja.seppli.learngym.model.Student;
 import ninja.seppli.learngym.model.Subject;
 import ninja.seppli.learngym.model.Teacher;
 
+/**
+ * Tests the student class
+ *
+ * @author sebi
+ *
+ */
 public class TestStudent {
 	private Course course;
 	private Subject math;
 	private Student student;
 
+	/**
+	 * sets up the environement
+	 */
 	@BeforeEach
 	public void setup() {
 		Teacher teacher = new Teacher("Franz", "Meier");
@@ -26,30 +35,72 @@ public class TestStudent {
 		course.addStudent(student);
 	}
 
+	/**
+	 * tests rounding of grades
+	 */
 	@Test
 	public void testRounding1() {
 		math.addGrade(student, 5);
 		math.addGrade(student, 5.5f);
-		assertEquals(5f, math.getAverage());
+		assertEquals(5f, student.getAverage());
 	}
 
+	/**
+	 * tests rounding of grades
+	 */
 	@Test
 	public void testRounding2() {
 		math.addGrade(student, 5);
 		math.addGrade(student, 6);
-		assertEquals(5.5f, math.getAverage());
+		assertEquals(5.5f, student.getAverage());
 	}
 
+	/**
+	 * tests rounding of grades
+	 */
 	@Test
 	public void testRounding3() {
 		math.addGrade(student, 5.5f);
 		math.addGrade(student, 6);
-		assertEquals(6f, math.getAverage());
+		assertEquals(6f, student.getAverage());
 	}
 
+	/**
+	 * tests adding grades
+	 */
 	@Test
 	public void testAddGrade() {
 		math.addGrade(student, 5.2f);
-		assertEquals(5f, math.getAverage());
+		assertEquals(5f, student.getAverage());
+	}
+
+	/**
+	 * tests prom/prov decision making
+	 */
+	@Test
+	public void testPromDecision1() {
+		math.addGrade(student, 3);
+		math.addGrade(student, 5);
+		math.addGrade(student, 5);
+		assertFalse(student.isProv());
+	}
+
+	/**
+	 * tests prom/prov decision making
+	 */
+	@Test
+	public void testPromDecision2() {
+		math.addGrade(student, 3);
+		math.addGrade(student, 5);
+		assertTrue(student.isProv());
+	}
+
+	/**
+	 * tests prom/prov decision making
+	 */
+	@Test
+	public void testPromDecision3() {
+		math.addGrade(student, 5);
+		assertTrue(student.isProv());
 	}
 }
--- src/test/java/ninja/seppli/learngym/TestSubject.java
+++ src/test/java/ninja/seppli/learngym/TestSubject.java
@@ -1,7 +1,6 @@
 package ninja.seppli.learngym;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,11 +10,20 @@ import ninja.seppli.learngym.model.Student;
 import ninja.seppli.learngym.model.Subject;
 import ninja.seppli.learngym.model.Teacher;
 
+/**
+ * Tests the subject class
+ * 
+ * @author sebi
+ *
+ */
 public class TestSubject {
 	private Course course;
 	private Subject math;
 	private Student student;
 
+	/**
+	 * sets up the environement
+	 */
 	@BeforeEach
 	public void setup() {
 		Teacher teacher = new Teacher("Franz", "Meier");
@@ -27,51 +35,42 @@ public class TestSubject {
 		course.addStudent(student);
 	}
 
+	/**
+	 * tests rounding of grades
+	 */
 	@Test
 	public void testRounding1() {
 		math.addGrade(student, 5);
 		math.addGrade(student, 5.5f);
-		assertEquals(5f, student.getAverage());
+		assertEquals(5f, math.getAverage());
 	}
 
+	/**
+	 * tests rounding of grades
+	 */
 	@Test
 	public void testRounding2() {
 		math.addGrade(student, 5);
 		math.addGrade(student, 6);
-		assertEquals(5.5f, student.getAverage());
+		assertEquals(5.5f, math.getAverage());
 	}
 
+	/**
+	 * tests rounding of grades
+	 */
 	@Test
 	public void testRounding3() {
 		math.addGrade(student, 5.5f);
 		math.addGrade(student, 6);
-		assertEquals(6f, student.getAverage());
+		assertEquals(6f, math.getAverage());
 	}
 
+	/**
+	 * tests adding grades
+	 */
 	@Test
 	public void testAddGrade() {
 		math.addGrade(student, 5.2f);
-		assertEquals(5f, student.getAverage());
-	}
-
-	@Test
-	public void testPromDecision1() {
-		math.addGrade(student, 3);
-		math.addGrade(student, 5);
-		math.addGrade(student, 5);
-		assertFalse(student.isProv());
-	}
-
-	@Test
-	public void testPromDecision2() {
-		math.addGrade(student, 3);
-		math.addGrade(student, 5);
-		assertTrue(student.isProv());
-	}
-
-	@Test
-	public void testPromDecision3() {
-		math.addGrade(student, 5);
-		assertTrue(student.isProv());
+		assertEquals(5f, math.getAverage());
 	}
 }
